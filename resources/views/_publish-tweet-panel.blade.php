644b7fb99a0ecc07b0502ec78b217c013b3081ce
Donika Tomiqi <donikatomiqi@gmail.com>
2020-04-16 21:03:55 +0200

    Design Timeline

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,10 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="" method="post">
+        <textarea name="body" class="w-full" placeholder="What's up..."></textarea>
+        <hr class="my-4">
+        <footer class="flex justify-between">
+            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full">
+            <button type="submit" class="bg-blue-500 rounded-lg p-2 text-white">Post</button>
+        </footer>
+    </form>
+</div>
