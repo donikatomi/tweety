f17ae35093ba78053b4f95aa0c45a49ed9507199
Donika Tomiqi <donikatomiqi@gmail.com>
2020-04-18 22:05:38 +0200

    Make the Timeline Dynamic

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,10 +1,16 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="" method="post">
+
+    <form action="/tweets" method="post">
+        @csrf
         <textarea name="body" class="w-full" placeholder="What's up..."></textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
-            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full">
+            <img src="{{ auth()->user()->getAvatarAtribute() }}" alt="your avatar" class="rounded-full">
             <button type="submit" class="bg-blue-500 rounded-lg p-2 text-white">Post</button>
         </footer>
     </form>
+
+    @error('body')
+    <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
