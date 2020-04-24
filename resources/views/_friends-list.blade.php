e42571b0dfff4e52c750e83b2529eff2b9721629
Donika Tomiqi <donikatomiqi@gmail.com>
2020-04-24 14:43:48 +0200

    Build a following

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,10 +1,10 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach (range(1,8) as $item)
+    @foreach (auth()->user()->follows as $user)
     <li class="mb-4">
         <div class="flex items-center text-sm">
-            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full">
-            <p class="ml-2">John Doe</p>
+            <img src="{{ $user->getAvatarAtribute() }}" alt="" class="rounded-full">
+            <p class="ml-2">{{ $user->name }}</p>
         </div>
     </li>
     @endforeach
